3175f4611a3a3fbd05a6efb9de9dc49b9cb7877d
Kil Matheus <kil.teixeira@sou.inteli.edu.br>
2023-03-18 13:09:42 -0300

    Comentário nos Códigos

--- src/data/receber.py
+++ src/data/receber.py
@@ -1,3 +1,4 @@
+# Classes: Coordenadas que vai receber os dados do formulário e vai inserir no banco de dados
 class Coordenadas():
     def __init__(self, id, x, y, z) -> None:
         self.id = id
--- src/main.py
+++ src/main.py
@@ -1,14 +1,16 @@
-import sqlite3
-from flask import Flask, render_template, request, redirect
-from data.receber import Coordenadas
+import sqlite3 # Importa o módulo sqlite3
+from flask import Flask, render_template, request, redirect # Importa o módulo Flask, junto com as bibliotecas utilizadas
+from data.receber import Coordenadas # Importa a classe Coordenadas do arquivo receber.py
 
+# Cria a aplicação Flask
 app = Flask(__name__)
 
-valor_index = 0
+# Variáveis globais que vão receber os dados para o banco de dados para ser exibido na página
 valor_coordenada = [
     Coordenadas(id = None, x = None, y = None, z = None)
 ]
 
+# Cria a rota inicial da aplicação, que vai renderizar o template index.html e exibir os dados do banco de dados
 @app.route("/")
 def index():
     db = sqlite3.connect("data/banco.db")
@@ -17,6 +19,7 @@ def index():
     valores = cursor.fetchall()
     db.close()
 
+#Loop que vai percorrer a lista de valores e vai atribuir os valores de cada posição para a classe Coordenadas. O For vai pegar a o 'valores[posição]' e vai receber a 'Coordenadas()' e dentro da classe vai receber os valores de cada posição segundo as suas posições dentro da array.
     for posicao in range(len(valores)):
         valores[posicao] = Coordenadas(
             id = valores[posicao][0],
@@ -26,6 +29,7 @@ def index():
         )
     return render_template("index.html", valores=valores)
 
+# Cria a rota que vai receber os dados do formulário e vai inserir no banco de dados
 @app.route("/criar", methods=["POST"])
 def numeros():
     valores = Coordenadas(
@@ -42,6 +46,6 @@ def numeros():
     valor_coordenada.append(valores)
     return redirect("/")
 
-
+# Cria a rota que vai receber os dados do formulário e vai atualizar no banco de dados
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000, debug=True)
